df175d4dcbb622529f40fa0aaa2ce851079f7ce6
mand2 <nayeon.kim1123@gmail.com>
2022-12-29 00:07:36 +0900

    [bj-1431] 시리얼 번호 수정

--- nayeon/recursion/bj_1431.py
+++ nayeon/recursion/bj_1431.py
@@ -8,10 +8,12 @@ nums = list(stdin.readline().rstrip() for _ in range(n))
 def sum_nums(x):
     s = 0
     for i in x:
-        s += 1 if i.isdigit() else 0
+        if i.isdigit():
+            s += int(i)
     return s
 
 
+# sorting 조건. 첫번째가 같다 -> 두번째 -> 세번째.
 nums.sort(key=lambda x: (len(x), sum_nums(x), x))
 
 print(*nums, sep='\n')
