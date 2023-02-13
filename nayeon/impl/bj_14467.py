bdd5aca14ea05d6df50c5b2312eb08d48a0cabad
mand2 <nayeon.kim1123@gmail.com>
2023-02-13 22:55:28 +0900

    [bj-14467] 소가 길을 건너간 이유 1

---
+++ nayeon/impl/bj_14467.py
@@ -0,0 +1,58 @@
+import sys;inp=sys.stdin.readline
+cows, cnt = {}, 0
+n=int(inp().rstrip())
+for _ in range(n):
+    idx, position = map(int, inp().rstrip().split())
+    if idx not in cows:
+        cows[idx] = position
+    elif idx in cows and cows[idx] != position:
+        cnt += 1
+        cows[idx] = position
+
+print(cnt)
+
+
+# short-coding
+import sys;inp=sys.stdin.readline
+cows, cnt = {}, 0
+n=int(inp().rstrip())
+for _ in range(n):
+    idx, position = map(int, inp().rstrip().split())
+    if idx in cows and cows[idx] != position:
+        cnt += 1
+    cows[idx] = position
+
+print(cnt)
+
+
+"""
+존이 할 일은 소가 길을 건너는 것을 관찰하는 것이다.
+존은 소의 위치를 N번 관찰하는데,
+각 관찰은 소의 번호와 소의 위치 하나씩으로 이루어져 있다.
+존은 소를 10마리 가지고 있으므로 소의 번호는 1 이상 10 이하의 정수고,
+소의 위치는 길의 왼쪽과 오른쪽을 의미하는 0과 1 중 하나다.
+
+이 관찰 기록을 가지고 소가 최소 몇 번 길을 건넜는지 알아보자. 즉 같은 번호의 소가 위치를 바꾼 것이 몇 번인지 세면 된다.
+
+
+
+첫 줄에 관찰 횟수 N이 주어진다.
+1 ≤ N ≤ 100 정수
+다음 N줄에는 한 줄에 하나씩 관찰 결과가 주어진다.
+관찰 결과는 소의 번호와 위치(0 또는 1)로 이루어져 있다.
+
+첫 줄에 소가 길을 건너간 최소 횟수를 출력한다.
+소 번호 1 ≤  ≤ 10
+
+8
+3 1
+3 0
+6 0
+2 1
+4 1
+3 0
+4 0
+3 1
+
+3
+"""
